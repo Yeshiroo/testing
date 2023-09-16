982369c48712e5bf5d5bbbf8bb233a2677fd0161
Yeshiroo <warrensenator10@gmail.com>
2023-09-16 20:04:21 +0800

    view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/Yeshiroo', 'Home::Yeshiroo');
+$routes->get('/product', 'ProductController::Yeshiroo');
+$routes->get('/product/(:any)', 'ProductController::product/$1');
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,14 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
+    public function Yeshiroo()
+    {
+        return view('products');
+    }
     public function index()
     {
         //
